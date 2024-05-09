a6bffa4cefec6646287f969f7cf6ea5e2acbac8d
= <=>
2024-05-08 22:17:57 -0300

    documentacion controller created

---
+++ laravel-carampague/app/Http/Controllers/DocumentacionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\documentacion;
+use Illuminate\Http\Request;
+
+class DocumentacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(documentacion $documentacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(documentacion $documentacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, documentacion $documentacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(documentacion $documentacion)
+    {
+        //
+    }
+}
