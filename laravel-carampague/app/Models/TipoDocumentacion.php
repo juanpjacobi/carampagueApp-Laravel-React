7a52545d0859db387650dabe5d1db26b3edd127a
= <=>
2024-06-17 17:12:52 -0300

    Model tipoDocumentacion created

---
+++ laravel-carampague/app/Models/TipoDocumentacion.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoDocumentacion extends Model
+{
+    protected $table = 'tipos_documentacion';
+    use HasFactory;
+}
