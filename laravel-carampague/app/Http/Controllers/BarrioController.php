526f460b71634d533f6482b731eac85bd5c5567d
= <=>
2024-06-17 17:02:29 -0300

    Barrio controller

---
+++ laravel-carampague/app/Http/Controllers/BarrioController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\BarrioCollection;
+use App\Models\Barrio;
+use Illuminate\Http\Request;
+
+class BarrioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $barrios = new BarrioCollection(Barrio::all());
+        return response(['barrios' => $barrios], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Barrio $barrio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Barrio $barrio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Barrio $barrio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Barrio $barrio)
+    {
+        //
+    }
+}
