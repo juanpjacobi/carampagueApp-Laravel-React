d91d1c77c01eaf81b72b6e4bede7e9f39331ef58
= <=>
2024-06-17 17:11:53 -0300

    Model provincia created

---
+++ laravel-carampague/app/Models/Prenda.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prenda extends Model
+{
+    protected $fillable = ['stock', 'tipo_prenda_id', 'talle_id'];
+
+
+    public function tipoPrenda()
+    {
+        return $this->belongsTo(TipoPrenda::class);
+    }
+    public function talle()
+    {
+        return $this->belongsTo(Talle::class);
+    }
+
+    use HasFactory;
+}
