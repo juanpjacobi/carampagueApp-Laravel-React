d4c10983ae8b4e0180e49d3e93af9e5c90d8ddeb
= <=>
2024-06-17 17:21:13 -0300

    barrel file  exports updated

--- react-carampague/src/views/index.js
+++ react-carampague/src/views/index.js
@@ -3,6 +3,15 @@ export { ActualizarAsociado } from "./asociados/ActualizarAsociado";
 export { CrearAsociado } from "./asociados/CrearAsociado";
 export { VerAsociado } from "./asociados/VerAsociado";
 
+export { DocumentacionAsociado } from "./asociados/Documentacion/DocumentacionAsociado";
+export { RegistrarDocumentacionAsociado } from "./asociados/Documentacion/RegistrarDocumentacionAsociado";
+export { EditarDocumentacion } from "./asociados/Documentacion/EditarDocumentacion";
+export { VerDocumentacion } from "./asociados/Documentacion/VerDocumentacion";
+
+export { EntregaRopa } from "./asociados/EntregaRopa/EntregaRopa";
+export { RegistrarEntregaRopa } from "./asociados/EntregaRopa/RegistrarEntregaRopa";
+export { EditarEntregaRopa } from "./asociados/EntregaRopa/EditarEntregaRopa";
+export { VerEntregaRopa } from "./asociados/EntregaRopa/VerEntregaRopa";
 
 export { ActualizarCliente } from "./clientes/ActualizarCliente";
 export { Clientes } from "./clientes/Clientes";
@@ -13,5 +22,3 @@ export { ActualizarObjetivo } from "./objetivos/ActualizarObjetivo";
 export { CrearObjetivo } from "./objetivos/CrearObjetivo";
 export { Objetivos } from "./objetivos/Objetivos";
 export { VerObjetivo } from "./objetivos/VerObjetivo";
-
-
