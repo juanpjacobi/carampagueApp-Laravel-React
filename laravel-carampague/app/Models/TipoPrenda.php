bacc0bd59303b6f68418c933a5b3da51bc3caeea
= <=>
2024-06-17 17:13:01 -0300

    Model tipo prenda created

---
+++ laravel-carampague/app/Models/TipoPrenda.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoPrenda extends Model
+{
+    protected $table = 'tipos_prenda';
+
+    use HasFactory;
+}
