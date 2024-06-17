0647a3f0d5984e7813e0586c8ff5006d5308f7a0
= <=>
2024-06-17 17:16:25 -0300

    components deleted

--- react-carampague/src/components/Alerta.jsx
+++
@@ -1,7 +0,0 @@
-export const Alerta = ({error}) => {
-  return (
-    <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
-    {error}
-  </span>
-  )
-}
--- react-carampague/src/components/Empty.jsx
+++
@@ -1,9 +0,0 @@
-
-export const Empty = ({message}) => {
-  return (
-    <div className="w-full text-center p-2">
-
-        <span className='text-sky-700 font-bold text-2xl'>{message}</span>
-    </div>
-  )
-}
