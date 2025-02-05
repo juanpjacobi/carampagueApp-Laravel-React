e43728e4c13c1d96c223c0fbd3763ed53a597956
= <=>
2025-02-05 09:03:53 -0300

    index routes updated

--- react-carampague/src/routes/index.js
+++ react-carampague/src/routes/index.js
@@ -1,4 +1,10 @@
+export { PlanDiarioRouter } from "./PlanDiarioRouter";
+export { ServicioRouter } from "./ServiciosRouter";
+
 export { ClienteRouter } from "./ClienteRouter";
 export { ObjetivoRouter } from "./ObjetivoRouter";
 export { UserRouter } from "./UserRouter";
 export { AsociadosRouter } from "./AsociadosRouter"
+export { ComputosRouter } from "./ComputosRouter"
+
+
