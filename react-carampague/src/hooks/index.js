f0889303f01714201b3335168b14b8adbb0d9204
= <=>
2025-02-05 09:03:30 -0300

    index hook updated

---
+++ react-carampague/src/hooks/index.js
@@ -0,0 +1,13 @@
+export { useValoresMapping } from "./useValoresMapping";
+
+export { useEsFeriado } from "./useEsferiado";
+
+export { useServicioObjetivo } from "./useServicioObjetivo";
+
+export { useAsociados } from "./useAsociados";
+export { useAuth } from "./useAuth";
+export { useHorasReales } from "./useHorasReales";
+export { useMotivos } from "./useMotivos";
+export { useValidacionLinea } from "./useValidacionLinea";
+export { useValorHora } from "./useValorHora";
+
