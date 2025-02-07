0bd126528413b90b9098c6a078015ed9765098be
= <=>
2025-02-07 09:41:41 -0300

    layout updated

--- react-carampague/src/layouts/Layout.jsx
+++ react-carampague/src/layouts/Layout.jsx
@@ -2,38 +2,9 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "../components/shared/NavBar";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  endInitialLoading,
-  startInitialLoading,
-} from "../store/slices/UiSlice";
+import { startInitialLoading, endInitialLoading } from "../store/slices/UiSlice";
 import { Spinner } from "../components/utilities/spinners/Spinner";
-import {
-  getAsociados,
-  getBarrios,
-  getClientes,
-  getCondicionesIva,
-  getDocumentaciones,
-  getEntregasRopa,
-  getEstadosCiviles,
-  getEstadosDocumentacion,
-  getLineasDocumentacion,
-  getLineasEntregaRopa,
-  getLocalidades,
-  getModalidades,
-  getObjetivos,
-  getPrendas,
-  getProvincias,
-  getServicios,
-  getTalles,
-  getTiposDocumentacion,
-  getTiposPrendas,
-  getTiposTelefonos,
-  getValores,
-} from "../store/thunks/zthunks";
-import { getLineasServicios } from "../store/thunks/LineasServiciosThunks";
-import { getMotivos } from "../store/thunks/MotivosThunks";
-import { getTiposMotivos } from "../store/thunks/TiposMotivosThunks";
-import { getFeriados } from "../store/thunks/FeriadosThunks";
+import { loadInitialResources } from "../store/thunks/LoadInitialResourcecs";
 
 export const Layout = () => {
   const dispatch = useDispatch();
@@ -43,33 +14,7 @@ export const Layout = () => {
     const fetchResources = async () => {
       dispatch(startInitialLoading());
       try {
-        await Promise.all([
-          dispatch(getServicios()),
-          dispatch(getLineasDocumentacion()),
-          dispatch(getCondicionesIva()),
-          dispatch(getEstadosCiviles()),
-          dispatch(getModalidades()),
-          dispatch(getEstadosDocumentacion()),
-          dispatch(getTiposDocumentacion()),
-          dispatch(getPrendas()),
-          dispatch(getTiposPrendas()),
-          dispatch(getTalles()),
-          dispatch(getValores()),
-          dispatch(getLocalidades()),
-          dispatch(getBarrios()),
-          dispatch(getProvincias()),
-          dispatch(getTiposTelefonos()),
-          dispatch(getEntregasRopa()),
-          dispatch(getDocumentaciones()),
-          dispatch(getClientes()),
-          dispatch(getObjetivos()),
-          dispatch(getAsociados()),
-          dispatch(getLineasEntregaRopa()),
-          dispatch(getLineasServicios()),
-          dispatch(getMotivos()),
-          dispatch(getTiposMotivos()),
-          dispatch(getFeriados()),
-        ]);
+        await loadInitialResources(dispatch);
       } finally {
         dispatch(endInitialLoading());
       }
@@ -82,8 +27,8 @@ export const Layout = () => {
   }
 
   return (
-    <main className="m-auto w-full overflow-auto flex flex-col  items-center pattern">
-      <div className="w-full border-b border-slate-300 shadow-lg  items-center flex flex-col lg:flex-row justify-between">
+    <main className="m-auto w-full overflow-auto flex flex-col items-center pattern">
+      <div className="w-full border-b border-slate-300 shadow-lg items-center flex flex-col lg:flex-row justify-between">
         <img src="/img/logo.jpg" alt="imagen logotipo" className="max-w-2xl" />
         <NavBar />
       </div>
