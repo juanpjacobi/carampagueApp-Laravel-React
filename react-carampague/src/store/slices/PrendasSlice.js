585f03e76ae4a78090f23a10c9e59928777cd306
= <=>
2025-02-17 08:29:35 -0300

    slice updated

--- react-carampague/src/store/slices/PrendasSlice.js
+++ react-carampague/src/store/slices/PrendasSlice.js
@@ -1,3 +1,4 @@
+// src/store/slices/PrendasSlice.js
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -17,9 +18,27 @@ export const PrendasSlice = createSlice({
       state.tiposPrendas = action.payload;
     },
     setTalles: (state, action) => {
-        state.talles = action.payload;
-      },
+      state.talles = action.payload;
+    },
+    addPrenda: (state, action) => {
+      state.prendas.push(action.payload);
+    },
+    updatePrendaInStore: (state, action) => {
+      const updatedPrenda = action.payload;
+      const index = state.prendas.findIndex((p) => p.id === updatedPrenda.id);
+      if (index !== -1) {
+        state.prendas[index] = updatedPrenda;
+      }
+    },
+    addTipoPrenda: (state, action) => {
+      state.tiposPrendas.push(action.payload);
+    },
+    addTalle: (state, action) => {
+      state.talles.push(action.payload);
+    },
   },
 });
 
-export const { setPrendas, setTipoPrendas, setTalles } = PrendasSlice.actions;
+export const { setPrendas, setTipoPrendas, setTalles, addPrenda, updatePrendaInStore, addTipoPrenda, addTalle } = PrendasSlice.actions;
+
+export default PrendasSlice.reducer;
