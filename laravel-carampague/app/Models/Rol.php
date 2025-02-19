a1519465e63d96c863a498ae85c2935fb3faea66
= <=>
2025-02-18 23:13:45 -0300

    model updated

--- laravel-carampague/app/Models/Rol.php
+++ laravel-carampague/app/Models/Rol.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rol extends Model
 {
+    protected $table = 'rols';
     use HasFactory;
 }
--- laravel-carampague/app/Models/User.php
+++ laravel-carampague/app/Models/User.php
@@ -17,10 +17,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
+
+     protected $fillable = [
+        'nombre_usuario',
         'password',
+        'rol_id',
+        'activo',
     ];
 
     /**
@@ -39,6 +41,8 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
+        'activo' => 'boolean',
+
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
