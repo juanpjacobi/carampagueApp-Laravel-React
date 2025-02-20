1430af4d5aab247ae2eaf650909b9d8a85d4a341
= <=>
2025-02-20 15:17:44 -0300

    schema updated

--- react-carampague/src/components/utilities/validator/asociado/userSchema.js
+++ react-carampague/src/components/utilities/validator/asociado/userSchema.js
@@ -4,13 +4,13 @@ export const userSchema = (editMode = false) =>
   Yup.object().shape({
 
     nombre_usuario: Yup.string()
-      .required("El nombre de usuario es obligatorio")
+      .required("Requerido")
       .max(255, "El nombre de usuario debe tener como máximo 255 caracteres"),
     password: editMode
       ? Yup.string() // Opcional en edición
       : Yup.string()
-          .required("La contraseña es obligatoria")
+          .required("Requerido")
           .min(6, "La contraseña debe tener al menos 6 caracteres"),
-    rol_id: Yup.string().required("El rol es obligatorio"),
-    activo: Yup.boolean().required("El estado es obligatorio"),
+    rol_id: Yup.string().required("Requerido"),
+    activo: Yup.boolean().required("Requerido"),
   });
